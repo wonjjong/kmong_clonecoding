af1d71062e7b51c090f2c9f2dd80c84612d0df10
wonjjong <wonjjong.dev@gmail.com>
2025-10-23 00:11:50 +0900

    section-1 레이아웃 구성

--- src/App.js
+++ src/App.js
@@ -4,29 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="header" style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', fontSize:'15px', paddingTop:'10px' }} >
-
-        <div style={{display: 'flex', alignItems: 'center'}}>
+      <header className="header">
+        <div>
           <span>이미지 영역</span>
-          <span style={{marginLeft:'20px'}}>
+          <span className="ml-20">
             크몽 Biz
           </span>
-          <button className="black-btn" style={{height:'25px', marginLeft:'10px'}}>토글버튼</button>
+          <button className="header-black-btn w-30 h-20 ml-10">토글버튼</button>
         </div>
 
-        <div style={{display: 'flex', alignItems: 'center'}}>
+        <div>
           <p>엔터프라이즈</p>
-          <div className="badge">기업용</div>
-          <span style={{marginLeft:'20px'}}>전문가 등록</span>
-          <button className="black-btn" style={{marginLeft:'20px'}}>회원가입</button>
+          <div className="header-badge">기업용</div>
+          <span className="ml-20">전문가 등록</span>
+          <span className="ml-20">로그인</span>
+          <button className="header-black-btn w-80 h-40 ml-20">회원가입</button>
         </div>
       </header>
 
-      <ul style={{display: 'flex', gap: '10px'}}>
-        <li className='list'></li>
-        <li className='list'></li>
-        <li className='list'></li>
-      </ul>
+      <section>
+        <div className="section-1">
+          <div className="section-1-left">
+            <span> 성공이 필요한 순간, <br/> 딱 맞는 전문가를 찾아보세요 </span>
+            <div className="search">
+              <input 
+                type="text" 
+                id="search-input" 
+                placeholder='검색하세용 검색!'/> 
+            </div>
+          </div>
+
+          <div className="section-1-right" style={{backgroundColor:'black'}}></div>          
+        </div>
+      </section>
+
+      <footer style={{backgroundColor:'red', marginTop: '10px'}}>
+        푸터푸터푸터
+      </footer>
     </div>
   );
 }
