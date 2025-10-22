c187571d6b6af256ec71ab9eb19fa3da9325f150
원종민 <won.dev@mobidoo.co.kr>
2025-10-22 11:22:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul style={{display: 'flex', gap: '10px'}}>
+        <li className='list'></li>
+        <li className='list'></li>
+        <li className='list'></li>
+      </ul>
     </div>
   );
 }
