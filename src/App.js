52e64c6224674dd58f47231597a7695a675f9486
wonjjong <wonjjong.dev@gmail.com>
2025-11-04 00:02:19 +0900

    layout 초기 구성 완료

--- src/App.js
+++ src/App.js
@@ -5,47 +5,66 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <div>
-          <span>이미지 영역</span>
-          <span className="ml-20">
-            크몽 Biz
-          </span>
-          <button className="header-black-btn w-30 h-20 ml-10">토글버튼</button>
-        </div>
-
-        <div>
-          <p>엔터프라이즈</p>
-          <div className="header-badge">기업용</div>
-          <span className="ml-20">전문가 등록</span>
-          <span className="ml-20">로그인</span>
-          <button className="header-black-btn w-80 h-40 ml-20">회원가입</button>
-        </div>
+          헤더헤더헤더
       </header>
 
-      <section>
-        <div className="section-1">
-          <div className="section-1-left">
-            <span> 성공이 필요한 순간, <br/> 딱 맞는 전문가를 찾아보세요 </span>
-            <div className="search">
-              <input 
-                type="text" 
-                id="search-input" 
-                placeholder='검색하세용 검색!'/> 
-            </div>
-            <div className="category mt-20">
-              <div className="border-denim background-denim"><p>카페24</p></div>
-              <div className="border-denim background-denim"><p>퍼블리싱</p></div>
-              <div className="ml-10"><p>홈페이지제작</p></div>
-              <div><p>카페24</p></div>
-              <div><p>홈페이지</p></div>
-              <div><p>워드프레스</p></div>
-            </div>
+      <main className="bg-gray pl-20 pr-20 pb-40 pt-40">
+        <section>
+          <div className="main-section-1" style={{background:'white'}}>
+              <div className="mt-20">
+                <span> 맥도날드 </span>
+                <h2 className="mt-10"> 치즈버거 세트</h2>
+                <div className="badge mt-20"> 사용완료 </div>
+                <img className="mt-40" src="https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg" 
+                  style={{width:'200px', height:'200px'}} alt="노트북 작업" />
+              </div>
           </div>
+        </section>
 
-          <div className="section-1-right" style={{backgroundColor:'black'}}></div>          
-        </div>
-      </section>
+        <section className="main-section-2 mt-40">
+            <div className="title">
+              상품 유의사항을 확인해 주세요!
+            </div>
 
+            <div className="description mt-8">
+              <span>
+                [상품설명] <br/>
+                스타벅스의 깔끔한 맛을 자랑하는 커피로, 스타벅스 파트너들이 가장 좋아하는 커피입니다.
+              </span>
+              <span>
+                [유의사항] <br/>
+                해당 쿠폰은 일부 점포에서는 취급하지 않는 상품일 수 있습니다. <br/>
+                상기 이미지는 연출된 것으로 실제와 다를 수 있습니다. <br/>
+                본 상품은 매장 재고 상황에 따라 동일 상품으로 교환이 불가능 할 수 있습니다.
+              </span>
+
+            </div>
+        </section>
+
+        <section className="main-section-3 mt-40">
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+              <span>교환처</span>
+              <span></span>
+              <span>맥도날드</span>
+            </div>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+              <span>공급자</span>
+              <span></span>
+              <span>주식회사 케이티알파</span>
+            </div>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+              <span>문의처</span>
+              <span></span>
+              <span>기프티쇼 비즈 1588-0108</span>
+            </div>
+            <div style={{display:'flex', justifyContent:'space-between'}}>
+              <span>사업자</span>
+              <span></span>
+              <span>(주)노티플러스</span>
+            </div>
+        </section>
+      </main>
+      
       <footer style={{backgroundColor:'red', marginTop: '10px'}}>
         푸터푸터푸터
       </footer>
